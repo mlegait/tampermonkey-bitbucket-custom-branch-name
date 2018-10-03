8ac25c7cd9f756a09805284820f889a9949ef983
mlegait <michele.legait+github@gmail.com>
2018-10-03 10:34:03 +0200

    Update the script to handle the last version of BitBucket

--- customBranchName.js
+++ customBranchName.js
@@ -1,36 +1,41 @@
 // ==UserScript==
 // @name         Bitbucket: Custom branch name
-// @version      1.0
+// @version      1.1
 // @match        https://bitbucket.org/branch/create*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-  'use strict';
-  const issueKey = (window.location.href.match(/\?issueKey=([^&amp;]+)/) || [])[1];
-  const issueType = (window.location.href.match(/&issueType=([^&]+)&/) || [])[1];
-  const issueSummary = (document.getElementById('id_branch_name').value.match(/[^-]+-\d+-(.*)/) || [])[1];
+  "use strict";
+  const issueKey = (window.location.href.match(/\?issueKey=([^&amp;]+)/) ||
+    [])[1];
+  const issueType = (window.location.href.match(/&issueType=([^&]+)&/) ||
+    [])[1];
+  const branchNameInput = document.getElementsByName("branchName")[0];
+  const issueSummary = (branchNameInput.value.match(/[^-]+-\d+-(.*)/) || [])[1];
 
-  let type = '';
+  let type = "";
   let key = issueKey;
-  switch(issueType)
-  {
-      case 'Bug':
-          type = 'patches';
-          break;
-      case 'Sub-task':
-          type = 'subtasks';
-          key= `<parent_issue_code>/${key}`;
-          break;
-      case 'Sub-bug':
-          type = 'patches';
-          key= `<parent_issue_code>/${key}`;
-          break;
-      default:
-          type = 'features';
-          break;
+  switch (issueType) {
+    case "Bug":
+      type = "patches";
+      break;
+    case "Sub-task":
+      type = "subtasks";
+      key = `<parent_issue_code>/${key}`;
+      break;
+    case "Sub-bug":
+      type = "patches";
+      key = `<parent_issue_code>/${key}`;
+      break;
+    default:
+      type = "features";
+      break;
   }
-  const summary = issueSummary.replace(/-/g , "_");
+  const summary = issueSummary.replace(/-/g, "_");
 
-  $('#id_branch_name').val(`${type}/${key}/${summary}`).trigger('change');
+  setTimeout(() => {
+    branchNameInput.value = `${type}/${key}/${summary}`;
+    branchNameInput.onchange();
+  }, 1000);
 })();
